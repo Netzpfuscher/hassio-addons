353fbe6b1d0c8ab9bff7fbda8bd6f98bc98b6ed9
Jens Kerrinnes <jens.kerrinnes@gmail.com>
2024-04-09 15:52:07 +0200

    Update run.sh

--- hamc-server-java/rootfs/etc/cont-init.d/run.sh
+++ hamc-server-java/rootfs/etc/cont-init.d/run.sh
@@ -3,10 +3,10 @@
 bashio::log.info "Starting..."
 
 if [ ! -d "/share/minecraft" ]; then
-  mkdir -p /share/minecraft
+  mkdir -p /share/minecraft/maps
 fi
 rm -rf /data/World
 
-ln -s /share/minecraft /data/World
+ln -s /share/minecraft/maps /data/maps
 
 /start
